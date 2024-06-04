271594a9f10a8e55ea4fb09f9bcc7058712f67f5
Antropov Anton <antropov.a.g@gmail.com>
2024-06-04 16:22:40 +0400

    Add header component (unauthorized version)

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,18 +3,27 @@ import styles from './header.module.css'
 export default function Header() {
     return (
         <header className={styles.container}>
-            <img className={styles.image} src="./logo.svg" alt="logo" />
-            <nav>
-                <ul>
-                    <li><a href="#">Главная</a></li>
-                    <li><a href="#">Тарифы</a></li>
-                    <li><a href="#">FAQ</a></li>
+            <div className={styles.imgContainer}>
+                <img className={styles.image} src="./logo.svg" alt="logo" />
+            </div>
+
+            <nav className={styles.navigation}>
+                <ul className={styles.navList}>
+                    <li><a className={styles.link} href="#">Главная</a></li>
+                    <li><a className={styles.link} href="#">Тарифы</a></li>
+                    <li><a className={styles.link} href="#">FAQ</a></li>
                 </ul>
             </nav>
-            <div>
-                <button>Зарегистрироваться</button>
-                <button>Войти</button>
+
+            <div className={styles.panelContainer}>
+                <div></div>
+                <div className={styles.loginPanel}>
+                    <button className={`${styles.button} ${styles.registerButton}`}>Зарегистрироваться</button>
+                    <div className={styles.divider}></div>
+                    <button className={styles.button}>Войти</button>
+                </div>
             </div>
+
         </header>
     )
 }
